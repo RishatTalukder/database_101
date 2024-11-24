10417e387e480997d83b9b090d3582a8454b7ea4
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2024-11-24 22:18:51 +0600

    .

--- basics/db.py
+++ basics/db.py
@@ -3,8 +3,16 @@ con = sqlite3.connect("tutorial.db")
 
 cur = con.cursor()
 
-# cur.execute("CREATE TABLE movie(title, year, score)") 
+# cur.execute("CREATE TABLE movie(title TEXT, year INTEGER, score REAL)") 
+
+cur.execute("INSERT INTO movie VALUES ('PRISONERS', '10', '9')")
 
 res = cur.execute("SELECT * FROM movie") ## read all data from movie table
+
+## we can add any datatype 
+## not permanently stored
+
+con.commit()
+
 
 print(res.fetchall())
